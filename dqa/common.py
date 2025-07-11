785031eb7a91fa720197d1388908a618ae606999
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-07-11 22:01:03 +0900

    chore: Upgraded dependencies. chore: Added context to icecream debugger.

--- dqa/common.py
+++ dqa/common.py
@@ -1,5 +1,7 @@
 try:
     from icecream import ic
+
+    ic.configureOutput(includeContext=True)
 except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
